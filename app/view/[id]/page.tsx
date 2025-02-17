e9bb44105fa9be7ec93c89741874b34718794764
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-02-17 05:07:58 -0500

    feat: implemented PDF view

---
+++ app/view/[id]/page.tsx
@@ -0,0 +1,54 @@
+import dynamic from "next/dynamic"
+import { getFileUrl } from "@/server/file"
+
+const PDFViewer = dynamic(() => import("@/components/PDFViewer"), { ssr: false })
+
+if (typeof Promise.withResolvers === "undefined") {
+  if (typeof window !== 'undefined') {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    window.Promise.withResolvers = function () {
+      let resolve, reject
+      const promise = new Promise((res, rej) => {
+        resolve = res
+        reject = rej
+      })
+      return { promise, resolve, reject }
+    }
+  } else {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    global.Promise.withResolvers = function () {
+      let resolve, reject
+      const promise = new Promise((res, rej) => {
+        resolve = res
+        reject = rej
+      })
+      return { promise, resolve, reject }
+    }
+  }
+}
+
+
+export default async function ViewPDF({ params }: { params: { id: string } }) {
+  try {
+    const { url } = await getFileUrl(params.id)
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-12 bg-zinc-100/50">
+        <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">PDF Viewer</h1>
+        <div className="p-1.5 bg-white rounded-sm w-full max-w-2xl border border-muted">
+          <PDFViewer fileUrl={url} />
+        </div>
+      </main>
+    )
+  } catch (error) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold mb-6 text-center text-red-500">Error</h1>
+          <p className="text-center">Failed to load PDF. Please try again.</p>
+        </div>
+      </main>
+    )
+  }
+}
+
---
+++ components/PDFViewer.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { Document, Page, pdfjs } from "react-pdf"
+import { Button } from "@/components/ui/button"
+import { ChevronLeft, ChevronRight, Dot, Pause, Play } from "lucide-react";
+import { parseAsInteger, useQueryState } from "nuqs"
+import { mergeTextItems } from "@/utils/functions";
+import { Skeleton } from "./ui/skeleton";
+import useTTS from "@/hooks/use-tts";
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+  import.meta.url
+).toString();
+
+//pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
+
+
+
+export default function PDFViewer({ fileUrl }: { fileUrl: string }) {
+  const [numPages, setNumPages] = useState<number | null>(null)
+  const [pageNumber, setPageNumber] = useQueryState("current_page", parseAsInteger.withDefault(1))
+  const [error, setError] = useState<string | null>(null);
+
+  const [pageText, setPageText] = useState("");
+  const { play, pause, isPlaying, isLoading, handleAudioPlayback } = useTTS({
+    pageNumber,
+    pageText,
+    documentId: "1",
+    voice: "s3://voice-cloning-zero-shot/baf1ef41-36b6-428c-9bdf-50ba54682bd8/original/manifest.json",
+    tempertaure: 0
+  })
+
+  function compilePageText(textItems: Array<{ str: string }>) {
+    const mergedText = mergeTextItems(textItems);
+    return setPageText(mergedText);
+  }
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    setNumPages(numPages)
+    setError(null)
+  }
+
+  function onDocumentLoadError(error: Error) {
+    console.error("Error loading PDF:", error)
+    setError("Failed to load PDF. Please try again.")
+  }
+
+
+
+  // if (error) {
+  //   return <div className="text-red-500">{error}</div>
+  // }
+
+  if (!fileUrl) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <Document renderMode="none" className="w-full"  file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} onLoadError={onDocumentLoadError}>
+        <div className="h-[500px] overflow-y-auto border border-red-500 w-full mx-auto">
+          <Page 
+            className="w-full" 
+            renderAnnotationLayer={false} 
+            renderTextLayer 
+            height={350}
+            width={500}
+            scale={1}
+            //onLoadSuccess={removeTextLayerOffset}
+            onGetTextSuccess={(content) => compilePageText(content.items as Array<{ str: string }>)} 
+            pageNumber={pageNumber} 
+          />
+        </div>
+      </Document>
+      <section className="flex items-center justify-center mt-4 space-x-4">
+        <Button 
+          onClick={handleAudioPlayback} 
+          disabled={isLoading}
+          size="icon" 
+          className="rounded-full"
+        >
+          {
+            isLoading ? <Dot className="size-4"/> :
+            isPlaying ? <Pause className="size-4" /> :
+            <Play className="size-4"/>
+          }
+        </Button>
+
+        <div className="flex items-center gap-4 px-2 py-1.5 bg-zinc-900 rounded-3xl text-white">
+          <Button 
+            size="icon"
+            variant="ghost"
+            className="rounded-full"
+            onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))} disabled={pageNumber <= 1}
+          >
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+          {
+            !numPages ? <Skeleton className="h-4 w-[80px]" /> :
+            <p className="text-sm">
+              Page {pageNumber} of {numPages}
+            </p>
+          }
+          <Button
+            size="icon"
+            variant="ghost"
+            className="rounded-full"
+            onClick={() => setPageNumber((prev) => Math.min(prev + 1, numPages || 1))}
+            disabled={pageNumber >= (numPages || 1)}
+          >
+            <ChevronRight className="h-4 w-4" />
+          </Button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
