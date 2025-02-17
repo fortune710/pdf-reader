f5d7ddaed9d1101bb117643ed9acff8fe53a2780
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-02-17 05:04:08 -0500

    imported file upload component using dynamic imports aka lazy loading for smaller bundle sizes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import dynamic from "next/dynamic";
+
+const FileUploader = dynamic(() => import("@/components/FileUploader"), { ssr: false });
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <h1 className="text-4xl font-bold text-white mb-8">PDF Uploader</h1>
+      <FileUploader />
+    </main>
+  )
+}
+
