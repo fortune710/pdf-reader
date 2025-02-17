1a80d0183601cae6e15a2721fbbf9becbeeefa8d
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-02-17 10:07:46 -0500

    feat: added loading spinner to improve UI for audio playback

---
+++ components/loading-spinner.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function LoadingSpinner() {
+    return (
+        <Image src="/90-ring.svg" alt="Loading spinner" width={20} height={20}/>
+    )
+}
